1ff8d7b8d3faec59fe79ca18370d431483e2e9d3
Chii Yuen <chii.yuen@hotmail.com>
2026-05-05 22:12:41 +0800

    Add tRPC server caller

---
+++ src/trpc/server.ts
@@ -0,0 +1,23 @@
+import { headers } from "next/headers";
+
+import { createTRPCContext } from "@/src/trpc/context";
+import { appRouter } from "@/src/trpc/routers/_app";
+
+function getRequestUrl(requestHeaders: Headers) {
+  const protocol = requestHeaders.get("x-forwarded-proto") ?? "http";
+  const host =
+    requestHeaders.get("x-forwarded-host") ?? requestHeaders.get("host") ?? "localhost:3000";
+
+  return `${protocol}://${host}/api/trpc`;
+}
+
+export async function createServerCaller() {
+  const incomingHeaders = await headers();
+  const requestHeaders = new Headers(incomingHeaders);
+  const req = new Request(getRequestUrl(requestHeaders), {
+    headers: requestHeaders,
+  });
+
+  const ctx = await createTRPCContext({ req });
+  return appRouter.createCaller(ctx);
+}
