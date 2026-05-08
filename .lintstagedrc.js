99bddc16422c846f3c78309452cdb3b217b679fd
Chii Yuen <chii.yuen@hotmail.com>
2026-05-08 16:04:47 +0800

    Fix oxlint and oxfmt exitting with error on no files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   "*.{js,jsx,ts,tsx,mjs,cjs}": (files) => [
-    `oxlint --fix ${files.join(" ")}`,
-    `oxfmt ${files.join(" ")}`,
+    `oxlint --fix --no-error-on-unmatched-pattern ${files.join(" ")}`,
+    `oxfmt --no-error-on-unmatched-pattern ${files.join(" ")}`,
   ],
 };
